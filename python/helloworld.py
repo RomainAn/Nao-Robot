3cebe209aca171dd6479f69c456336604f1112a1
Sean Guo <iguoxiaopeng@gmail.com>
2014-11-01 10:15:09 +0800

    nao robot init

---
+++ python/helloworld.py
@@ -0,0 +1,19 @@
+from naoqi import ALProxy
+#-*- coding: utf-8 -*-
+#必须要加上面一行代码，表示使用utf-8编码格式，否则python解释器不能识别汉字编码，运行将出现句法错误
+
+robot_ip = "192.168.1.100"
+robot_port = 9559	# default port : 9559
+
+# ALTextToSpeech is the module of NAoqi dedicated to speech. 
+# The say method makes the robot pronounce the string given in parameter.
+
+# tts is the name we gave to the object instance (could have been myspeechmodule or speakingmodule).
+# ALProxy() is a class of objects, allowing you to have acces to all the methods of a module.
+# ALTextToSpeech is the name of the module of NAOqi we want to use.
+# IP and Port (9559) of the robot are also specified (it was not the case with Choregraphe).
+tts = ALProxy("ALTextToSpeech", robot_ip, robot_port)
+
+# 根据语言包选择下面其中一条命令。英语语言包不能说汉语，同样汉语语言包不能说英语。
+#tts.say("Hello, world! I am Nao robot!")
+tts.say("黑化肥挥发发灰会花飞")
---
+++ python/set_python_path.sh
@@ -0,0 +1,15 @@
+#################################################################
+#   Copyright (C) 2014 Sean Guo. All rights reserved.
+#														  
+#	> File Name:        < set_python_path.sh >
+#	> Author:           < Sean Guo >		
+#	> Mail:             < iguoxiaopeng@gmail.com >		
+#	> Created Time:     < 2014/10/12 >
+#	> Last Changed: 
+#	> Description:
+#################################################################
+
+#!/bin/bash
+
+export PYTHONPATH=${PYTHONPATH}:~/Code/nao/python/pynaoqi-python2.7-2.1.0.19-mac64/
+export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:~/Code/nao/python/pynaoqi-python2.7-2.1.0.19-mac64/
