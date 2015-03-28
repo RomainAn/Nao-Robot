e838d61c769d418ac81d3113e73f735085dbb0cf
Sean Guo <iguoxiaopeng@gmail.com>
2015-03-28 08:32:09 +0800

    little

--- python/socket/xpserver/xpserver.py
+++ python/socket/xpserver/xpserver.py
@@ -317,48 +317,48 @@ def sensor(interval):
 	# SENSOR == False
 	thread.exit_thread()
 
-def LArmInit():
+def LArmInit():	# 配置Left Arm 的所有关节为初始位置0.
 	motion.setAngles('LShoulderPitch', 0, 0.2)
 	motion.setAngles('LShoulderRoll', 0, 0.2)
 	motion.setAngles('LElbowYaw', 0, 0.2)
 	motion.setAngles('LElbowRoll', 0, 0.2)
 	motion.setAngles('LWristYaw', 0, 0.2)
 	motion.setAngles('LHand', 0, 0.2)
-def RArmInit():
+def RArmInit(): # 配置Right Arm 的所有关节为初始位置0.
 	motion.setAngles('RShoulderPitch', 0, 0.2)
 	motion.setAngles('RShoulderRoll', 0, 0.2)
 	motion.setAngles('RElbowYaw', 0, 0.2)
 	motion.setAngles('RElbowRoll', 0, 0.2)
 	motion.setAngles('RWristYaw', 0, 0.2)
 	motion.setAngles('RHand', 0, 0.2)
-def LArmUp():
+def LArmUp(): # Left Arm 抬起
 	motion.setAngles('LShoulderPitch', 0.7, 0.2)
 	motion.setAngles('LShoulderRoll', 0.3, 0.2)
 	motion.setAngles('LElbowYaw', -1.5, 0.2)
 	motion.setAngles('LElbowRoll', -0.5, 0.2)
 	motion.setAngles('LWristYaw', -1.7, 0.2)
-def RArmUp():
+def RArmUp(): # Right Arm 抬起
 	motion.setAngles('RShoulderPitch', 0.7, 0.2)
 	motion.setAngles('RShoulderRoll', -0.3, 0.2)
 	motion.setAngles('RElbowYaw', 1.5, 0.2)
 	motion.setAngles('RElbowRoll', 0.5, 0.2)
 	motion.setAngles('RWristYaw', 1.7, 0.2)
 def ArmUp2():
-	# 简易版本
+	# 简易版本，依赖rest()/wakeUp()后的姿势。
 	motion.rest()
 	motion.wakeUp()
 	motion.setAngles('RShoulderPitch', 0.7, 0.2)
 	motion.setAngles('RWristYaw', 1.5, 0.2)
 	motion.setAngles('LShoulderPitch', 0.7, 0.2)
 	motion.setAngles('LWristYaw', -1.5, 0.2)
-def LArmMoveInit():
+def LArmMoveInit(): # 配置Left Arm 为行走初始化的姿态。
 	motion.setAngles('LShoulderPitch', 1, 0.2)
 	motion.setAngles('LShoulderRoll', 0.3, 0.2)
 	motion.setAngles('LElbowYaw', -1.3, 0.2)
 	motion.setAngles('LElbowRoll', -0.5, 0.2)
 	motion.setAngles('LWristYaw', 0, 0.2)
 	motion.setAngles('LHand', 0, 0.2)
-def RArmMoveInit():
+def RArmMoveInit(): # 配置Right Arm 为行走初始化的姿态。
 	motion.setAngles('RShoulderPitch', 1, 0.2)
 	motion.setAngles('RShoulderRoll', -0.3, 0.2)
 	motion.setAngles('RElbowYaw', 1.3, 0.2)
