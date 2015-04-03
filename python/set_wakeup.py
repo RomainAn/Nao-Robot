1d211cad97a444fcc9fc26d01c018002508c6808
Sean Guo <iguoxiaopeng@gmail.com>
2015-04-03 11:24:36 +0800

    change robot ip

--- python/set_wakeup.py
+++ python/set_wakeup.py
@@ -22,7 +22,7 @@ def main(robot_IP, robot_PORT=9559):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", type=str, default="192.168.1.100", help="Robot ip address")
+    parser.add_argument("--ip", type=str, default="192.168.2.100", help="Robot ip address")
     parser.add_argument("--port", type=int, default=9559, help="Robot port number")
     args = parser.parse_args()
 	# ----------> 执行main函数 <----------
--- python/template.py
+++ python/template.py
@@ -14,7 +14,10 @@
 import argparse
 from naoqi import ALProxy
 
+tts = motion = posture = memory = leds = battery = autonomous = None
+
 def main(robot_IP, robot_PORT=9559):
+	global tts, motion, posture, memory, leds, battery, autonomous
 	# ----------> Connect to robot <----------
 	tts = ALProxy("ALTextToSpeech", robot_IP, robot_PORT)
 	motion = ALProxy("ALMotion", robot_IP, robot_PORT)
@@ -29,7 +32,7 @@ def main(robot_IP, robot_PORT=9559):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", type=str, default="192.168.1.100", help="Robot ip address")
+    parser.add_argument("--ip", type=str, default="192.168.2.100", help="Robot ip address")
     parser.add_argument("--port", type=int, default=9559, help="Robot port number")
     args = parser.parse_args()
 	# ----------> 执行main函数 <----------
